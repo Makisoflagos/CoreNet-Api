c439b7b23e89a568e4776e8b363c23eabe32e571
RayAgu <aguye.raymond@gmail.com>
2023-08-09 12:37:42 +0100

    second update

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -3,13 +3,13 @@ const router = express.Router()
 const { createWriterComment, allWriterComments, singleWriterComment, updateWriterComment, deleteWriterComment, createEditorComment, singleEditorComment, allEditorComments, updateEditorComment, deleteEditorComment} = require('../controllers/commentsController');
 
 router.route("/writercomment").post( createWriterComment )
-router.route("/writercomment").post( updateWriterComment )
+router.route("/writercomment").put( updateWriterComment )
 router.route("/writercomment").get( allWriterComments )
 router.route("/writercomment").get( singleWriterComment )
 router.route("/writercomment").delete( deleteWriterComment )
 
 router.route("/editorcomment").post( createEditorComment )
-router.route("/editorcomment").post( updateEditorComment )
+router.route("/editorcomment").put( updateEditorComment )
 router.route("/editorcomment").get( allEditorComments )
 router.route("/editorcomment").get( singleEditorComment )
 router.route("/editorcomment").delete( deleteEditorComment )
